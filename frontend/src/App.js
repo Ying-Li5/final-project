15049b718ca296b7f0a4078fad10f76f76140771
Ying <ying__li@hotmail.com>
2023-04-30 22:15:21 -0400

    main page mock data+style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
+// insert routing here
+
 import './App.css';
 import React from 'react';
 import Nav from './Pages/Nav'
 import Login from './Pages/Login'
 import User from './Pages/User'
+import Main from './Pages/Main'
 
 export default function App() {
   return (
     <div className="App">
       <Nav />
       {/* <Login /> */}
-      <User />
+      {/* <User /> */}
+      <Main />
     </div>
   );
 }
--- frontend/src/Pages/Main.js
+++ frontend/src/Pages/Main.js
@@ -1,11 +1,80 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap/dist/js/bootstrap.bundle.min";
+import React, { useEffect } from "react";
+import style from './CSS/Main.css';
+import mockData from '../public/images/game.jpg';
+import { FaUserFriends } from "react-icons/fa";
+import { RiSignalTowerLine, RiFileList2Fill, RiPercentLine } from "react-icons/ri";
+import { HiOutlineClock, HiTag, HiPlus } from "react-icons/hi";
+import { IoLogoGameControllerB } from "react-icons/io";
+import { AiOutlineStock } from "react-icons/ai";
 
 export default function Main() {
+    useEffect(() => {
+        const url = `http://localhost:3000/game`
+        fetch(url, {
+            method: "GET",
+            headers: {'Content-Type': 'application/json'},
+        }).then((result) => {
+            result.json().then((resolvedResult) => console.log(resolvedResult))
+        })
+    }, [])
+
     return(
-        <div>
-            
+        <div className="main-grid-container">
+            <div className="grid-item-1">
+                <ul className="recommended">
+                    <li className="categories">RECOMMENDED</li>
+                    <li><FaUserFriends /> By Friends</li>
+                    <li><RiSignalTowerLine /> By Curators</li>
+                    <li><HiTag /> Tags</li>
+                </ul>
+
+                <ul>
+                    <li className="categories">DISCOVERY QUEUES</li>
+                    <li><RiFileList2Fill /> Recommendations</li>
+                    <li><HiPlus /> New Releases</li>
+                </ul>
+
+                <ul>
+                    <li className="categories">BROWSE CATEGORIES</li>
+                    <li><AiOutlineStock /> Top Sellers</li>
+                    <li><HiPlus /> New Releases</li>
+                    <li><HiOutlineClock /> Upcoming</li>
+                    <li><RiPercentLine /> Specials</li>
+                    <li><IoLogoGameControllerB /> VR Titles</li>
+                    <li><IoLogoGameControllerB /> Controller-Friendly</li>
+                </ul>
+
+                <ul>
+                    <li className="categories">BROWSE BY GENRE</li>
+                    <li>Free to Play</li>
+                    <li>Early Access</li>
+                    <li>Action</li>
+                    <li>Adventure</li>
+                    <li>Casual</li>
+                    <li>Indie</li>
+                    <li>Massively Multiplayer</li>
+                    <li>Racing</li>
+                    <li>RPG</li>
+                    <li>Simulation</li>
+                    <li>Sports</li>
+                    <li>Strategy</li>
+                </ul>
+            </div>
+
+            <div className="grid-item-2">
+                <h6 className='content-title'>FEATURED AND RECOMMENDED</h6>
+                <img className="game-data" src={mockData} alt="game-data" />
+            </div>
+
+            <div className="grid-item-3">
+                <h6 className='content-title-1'>SPECIAL OFFERS</h6>
+                <img className="game-data-1" src={mockData} alt="game-data" />
+            </div>
+
+            <div className="grid-item-4">
+                <h6 className='content-title-2'>BROWSE BY CATEGORIES</h6>
+                <img className="game-data-2" src={mockData} alt="game-data" />
+            </div>
         </div>
     )
 }
