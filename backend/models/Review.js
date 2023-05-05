857d8c5ae5730c962b825535574d2278d8c34b0d
Ying <ying__li@hotmail.com>
2023-05-04 23:09:30 -0400

    updating review models + seeds

--- backend/models/Review.js
+++ backend/models/Review.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
 const ReviewSchema = new mongoose.Schema({
-    author_id: { type: String },
+    username: { type: String },
     score: { type: Number },
     game_id: { type: String },
     title: { type: String },
     content: { type: String },
+    createdAt: { type: Date },
 }) 
 
 const Review = mongoose.model("Review", ReviewSchema)
--- backend/seeds/seed.js
+++ backend/seeds/seed.js
@@ -1,6 +1,4 @@
-const Game = require("../models/Game.js")
 const Review = require("../models/Review.js")
-const User = require("../models/User.js")
 const mongoose = require("mongoose")
 
 async function seed() {
@@ -8,89 +6,32 @@ async function seed() {
     useNewUrlParser: true, 
     useUnifiedTopology: true
   })
-  
-  const game = await new Game({
-      description: "This is the first game",
-      average_score: 10,
-      title: "Risk of Rain",
-      year: 2018,
-      art: "https://upload.wikimedia.org/wikipedia/en/c/c1/Risk_of_Rain_2.jpg"
-  }).save()
-  
-  const game2 = await new Game({
-      description: "This is the second game",
-      average_score: 10,
-      title: "Lobotomy Corporation",
-      year: 2014,
-      art: "https://static-cdn.jtvnw.net/ttv-boxart/457724187_IGDB-272x380.jpg"
-  }).save()
-
-  const game3 = await new Game({
-    description: "This is the third game",
-    average_score: 9,
-    title: "Resident Evil 2",
-    year: 2019,
-    art: "https://upload.wikimedia.org/wikipedia/en/thumb/f/fd/Resident_Evil_2_Remake.jpg/220px-Resident_Evil_2_Remake.jpg"
-  }).save()
-
-const game4 = await new Game({
-  description: "This is the fourth game",
-  average_score: 7,
-  title: "No Man's Sky",
-  year: 2016,
-  art: "https://upload.wikimedia.org/wikipedia/en/6/67/No_Man%27s_Sky.jpg"
-}).save()
-
-const game5 = await new Game({
-  description: "This is the fifth game",
-  average_score: 10,
-  title: "God of War Ragnarok",
-  year: 2023,
-  art: "https://image.api.playstation.com/vulcan/ap/rnd/202207/1210/4xJ8XB3bi888QTLZYdl7Oi0s.png"
-}).save()
-  
-  const user = await new User({
-    username: "htoomin",
-    password: "Welcome1",
-    name: "Htoo Min"
-  }).save()
-  
-  const user2 = await new User({
-    username: "yingli",
-    password: "Welcome1",
-    name: "Ying Li"
-  }).save()
-  
-  const review = await new Review({
-    author_id: user._id.toString(),
-    score: 4,
-    game_id: game2._id.toString(),
-    title: "This game made my life harder",
-    content: "Why would anyone play a game like this ever in their life. The game just gets harder and harder! It has invaded my nightmares at this point"
-  }).save()
 
   const review1 = await new Review({
-    author_id: user._id.toString(),
+    username: "Shorgi",
     score: 8,
-    game_id: game2._id.toString(),
+    game_id: "6452dc52d5a54c84c76d3e6c",
     title: "This game made my life harder...I love it!",
-    content: "Peh"
+    content: "Peh",
+    createdAt: new Date()
   }).save()
 
   const review2 = await new Review({
-    author_id: user._id.toString(),
+    username: "Florgi",
     score: 5,
-    game_id: game2._id.toString(),
+    game_id: "6452dc52d5a54c84c76d3e6c",
     title: "Easier than a fulltime Job",
-    content: "It's like having a fulltime job! I worry about it constantly and can't sleep at night"
+    content: "It's like having a fulltime job! I worry about it constantly and can't sleep at night",
+    createdAt: new Date()
   }).save()
 
   const review3 = await new Review({
-    author_id: user._id.toString(),
+    username: "Corgi",
     score: 4,
-    game_id: game2._id.toString(),
+    game_id: "6452dc52d5a54c84c76d3e6c",
     title: "Ying hates it i love it",
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    createdAt: new Date()
   }).save()
 }
 
